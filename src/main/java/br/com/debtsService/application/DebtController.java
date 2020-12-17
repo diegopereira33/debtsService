fcf385f525473bca5723240b652ef08e483b4da0
disantosfh <disantos@fh.com.br>
2020-12-17 00:16:53 -0300

    enable cors

--- src/main/java/br/com/debtsService/application/DebtController.java
+++ src/main/java/br/com/debtsService/application/DebtController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,44 +32,51 @@ public class DebtController {
 		this.service = service;
 	}
 	
+	@CrossOrigin
 	@GetMapping
 	public List<Debt> findAll() {
 		return service.findAll();
 	}
 	
+	@CrossOrigin
 	@GetMapping("/{id}")
 	public Debt findById(@PathVariable Long id) {
 		return service.findById(id)
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Debt not found"));
 	}
 	
+	@CrossOrigin
 	@GetMapping("/name/{name}")
 	public List<Debt> findByName(@PathVariable String name) {
 		return service.findByName(name);
 	}
 	
+	@CrossOrigin
 	@GetMapping("/date/{initialDate}/{finalDate}")
 	public List<Debt> findByDate(@PathVariable String initialDate, @PathVariable String finalDate ) {
 		return service.findByDate(initialDate, finalDate);
 	}
 
 
+	@CrossOrigin
 	@GetMapping("/category/{category}")
 	public List<Debt> findByCategory(@PathVariable String category) {
 		return service.findByCategory(category);
 	}
 
-	
+	@CrossOrigin
 	@PostMapping
 	public Debt create(@RequestBody @Valid Debt debt) {
 		return service.create(debt);
 	}
 	
+	@CrossOrigin
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Debt> update(@PathVariable("id") long id, @RequestBody @Valid Debt debt) {
 		return service.update(id, debt);
 	}
 	
+	@CrossOrigin
 	@DeleteMapping(path = { "/{id}" })
 	public Optional<?> delete(@PathVariable long id) {
 		service.findById(id)
